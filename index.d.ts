2540c2b4fb865723cb1290c6fe2cc09c2415be85
technicalopt <47841277+technicalopt@users.noreply.github.com>
2020-02-17 14:17:07 +0700

    Update support typescript

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
 declare module "react-native-screenshot-detect" {
     import { NativeEventEmitter } from 'react-native';
 
-    export function addEventListener(cb: Function): NativeEventEmitter;
-    export function startListener(): void;
-    export function stopListener(): void;
-    export function clearListener(): void;
+    export default {
+        addEventListener(cb: Function): NativeEventEmitter,
+        startListener(): void,
+        stopListener(): void,
+        clearListener(): void
+    }
 }
